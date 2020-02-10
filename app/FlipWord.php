194ea388cfbeef4aac29ea6fcb23ee3f7e934cd8
Christophe BRUYERE <christophe.developpement@gmail.com>
2020-02-10 18:25:59 +0100

    Add delimiter parameter to FlipWord function

--- app/FlipWord.php
+++ app/FlipWord.php
@@ -4,15 +4,16 @@
  * Reverse each word in a string.
  *
  * @param string $inputString The string to be reverse
+ * @param string $delimiter The words delimiter default to space
  *
  * @return string
  */
-function reverseWord(string $inputString) : string
+function reverseWord(string $inputString, string $delimiter = ' ') : string
 {
     // we accept that input string contains a space character between each word
     $finalWordsArray = [];
 
-    foreach (explode(' ', $inputString) as $word) {
+    foreach (explode($delimiter, $inputString) as $word) {
         array_push($finalWordsArray, strrev($word));
     }
 
